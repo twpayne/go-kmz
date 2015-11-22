2c42eae8f63d525669d3e55e1882f5eca25087e9
Tom Payne <twpayne@gmail.com>
2015-11-22 21:30:29 +0100

    Initial commit

---
+++ kmz.go
@@ -0,0 +1,55 @@
+// Package kmz provides convenience methods for creating and writing KMZ files.
+package kmz
+
+import (
+	"archive/zip"
+	"io"
+
+	"github.com/twpayne/go-kml"
+)
+
+type KMZ struct {
+	roots []kml.Element
+	files map[string][]byte
+}
+
+func NewKMZ(roots ...kml.Element) *KMZ {
+	return &KMZ{
+		roots: roots,
+		files: make(map[string][]byte),
+	}
+}
+
+func (kmz *KMZ) AddRoot(root kml.Element) *KMZ {
+	kmz.roots = append(kmz.roots, root)
+	return kmz
+}
+
+func (kmz *KMZ) AddFile(filename string, content []byte) *KMZ {
+	kmz.files[filename] = content
+	return kmz
+}
+
+func (kmz *KMZ) Write(w io.Writer) error {
+	zw := zip.NewWriter(w)
+	f, err := zw.Create("doc.kml")
+	if err != nil {
+		return err
+	}
+	if err := kml.KML(kml.Document(kmz.roots...)).Write(f); err != nil {
+		return err
+	}
+	for filename, content := range kmz.files {
+		f, err := zw.Create(filename)
+		if err != nil {
+			return err
+		}
+		if _, err := f.Write(content); err != nil {
+			return err
+		}
+	}
+	if err := zw.Close(); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ kmz_test.go
@@ -0,0 +1,24 @@
+package kmz
+
+import (
+	"bytes"
+	"log"
+
+	"github.com/twpayne/go-kml"
+)
+
+func ExampleNewKMZ() {
+	kmz := NewKMZ(
+		kml.Placemark(
+			kml.Name("Simple placemark"),
+			kml.Description("Attached to the ground. Intelligently places itself at the height of the underlying terrain."),
+			kml.Point(
+				kml.Coordinates(kml.Coordinate{Lon: -122.0822035425683, Lat: 37.42228990140251}),
+			),
+		),
+	)
+	w := &bytes.Buffer{}
+	if err := kmz.Write(w); err != nil {
+		log.Fatal(err)
+	}
+}
